884c002e7fbe09ad5128e7601d7ccc9b409007e1
student <student@imac-139.local>
2018-11-24 15:33:57 +0800

    Initial Commit

---
+++ DynamicDemo8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicDemo8
+//
+//  Created by student on 2018/11/24.
+//  Copyright © 2018年 2016110449. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
